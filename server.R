6d6628d801f24126b207ed657585e8008c3699df
Ning <ln1267@gmail.com>
2019-10-31 17:27:11 -0400

    intial shiny app

---
+++ server.R
@@ -0,0 +1,420 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# if ("devtools" %in% installed.packages()) {library(devtools)} else{install.package("devtools") }
+
+# function for checking libs
+f_lib_check<-function(libs){
+  for (lib in libs ){
+    if(lib %in% rownames(installed.packages())){
+
+    }else{
+      install.packages(lib,repos='http://cran.us.r-project.org')
+    }
+  }
+
+  a<-lapply(libs, require, character.only = TRUE)
+}
+theme_ning<-function(size.axis=5,size.title=6){
+  theme_bw(base_family = "serif") %+replace%
+    theme(axis.title = element_text(face="bold", colour="black", size=size.title),
+          axis.text= element_text(angle=0, vjust=0.3, size=size.axis),
+          legend.title = element_text(colour="black", size=size.axis, face="bold"),
+          legend.text = element_text(colour="black", size = size.axis),
+          strip.text.x = element_text(size = size.axis,margin=margin(4, 2, 6, 2), face="bold"),
+          strip.text.y = element_text(size = size.axis,margin=margin(4, 2, 4, 6), face="bold",angle=-90),
+          legend.key.size=unit(1.2, "lines"),
+          legend.box.spacing=unit(1, "mm"),
+          strip.background = element_blank(),
+          plot.title = element_text(vjust = 2.5,hjust = 0.5,face="bold")
+    )
+}
+# librs<-c("plyr","bfast","ggplot2","ggrepel","reshape2","pryr","ncdf4",
+#          "caTools","graphics","parallel","zoo","RColorBrewer",
+#          "trend","gmodels","vcd","abind","Evapotranspiration","chron",
+#          "xts","dplyr","hydroGOF")
+
+librs<-c("dplyr","raster","ggplot2","leaflet","rgdal")
+f_lib_check(librs)
+
+# # load revised "hydromad" package
+# if (!"hydromad" %in% installed.packages()) install_github("ln1267/hydromad")
+# load required libraries
+#library(hydromad)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output,session) {
+
+  # Tab: Upload data----
+  # Sidepanel: upload session ----
+  ## file2plot: Select a uploaded file for plotting----
+  observe({
+    x <- "test.csv"
+    if(!is.null(input$files$name)) x<-input$files$name
+
+    # Can also set the label and select items
+    updateSelectInput(session, "file2plot",
+                      label = paste("Select input label", length(x)),
+                      choices = x,
+                      selected = tail(x, 1)
+    )
+  })
+  BasinShp<-NULL
+  ## Read Basin shapefile ----
+  observeEvent(input$Input_basin,{
+    BasinShp<<- readOGR(input$Input_basin$datapath)
+  })
+
+  ## Plot Basin ----
+  observeEvent(input$plotbasin,{
+  output$basinmap <- renderLeaflet({
+    if(is.null(BasinShp)) return()
+    popup = BasinShp$Station
+    leaflet()  %>% addTiles() %>%
+      #setView(lng = llong, lat=llat,zoom=13) %>%
+      addPolygons(data=BasinShp,weight=1,col = 'black',fillOpacity = 0.2,fillColor = BasinShp$Station,
+                  highlight = highlightOptions(color='white',weight=1,
+                                               bringToFront = TRUE), label= popup)
+  })
+})
+
+  #data_input<<-list()
+  ## Action: Print the uploaded file names ----
+  observeEvent(input$readdata,{
+
+                print(input$Input_climate)
+                if (!is.null(input$Input_climate)) data_input[["Climate"]]<<-read.csv(input$Input_climate$datapath)
+                if (!is.null(input$Input_LAI)) data_input[["LAI"]]<<-read.csv(input$Input_LAI$datapath)
+                if (!is.null(input$Input_landinfo)) data_input[["Landinfo"]]<<-read.csv(input$Input_landinfo$datapath)
+                if (!is.null(input$Input_HRUinfo)) data_input[["HRUinfo"]]<<-read.csv(input$Input_HRUinfo$datapath)
+
+                output$printsummary<-renderPrint({
+                 for (i in 1:length(data_input)){
+                   var<-names(data_input)[i]
+                   df<-data_input[[i]]
+                   print(paste0(var," data----"))
+                   print(head(df))
+                   print(summary(df))
+                  }
+                })
+           })
+
+
+  ## output$distPlot: Plot the select dataset ----
+  output$Plotinput <- renderPlot({
+
+    if(input$plotdata>0){
+      print(paste0("Print data ....",input$daname2plot))
+      df<-data_input[[input$daname2plot]]%>%
+          filter(Year>=input$plotyrrange[1] & Year<=input$plotyrrange[2])%>%
+          filter(Month %in% input$plotmonths)%>%
+          mutate(Date=as.Date(paste0(Year,"-",Month,"-","01")))%>%
+          dplyr::select(-Year,-Month)
+
+      if(input$daname2plot == "Climate"){
+
+        names(df)<-c("HRU_ID","Precip","Tmean","Date")
+        df$HRU_ID<-factor(df$HRU_ID)
+        ggplot(df,aes(x=Date,y=Precip,col=HRU_ID))+geom_line()+geom_point()+
+          ggtitle(paste0("Monthly Precipitation (mm/month)"))+
+          facet_wrap(HRU_ID~.,ncol=3)+
+          theme_ning(size.axis = 12,size.title = 14)
+      }else if(input$daname2plot == "LAI"){
+        names(df)<-c("HRU_ID","LAI","Date")
+        df$HRU_ID<-factor(df$HRU_ID)
+        ggplot(df,aes(x=Date,y=LAI,col=HRU_ID))+geom_line()+geom_point()+
+          ggtitle(paste0("Monthly Leaf area index (m2/m2)"))+
+          facet_wrap(HRU_ID~.,ncol=3)+
+          theme_ning(size.axis = 12,size.title = 14)
+
+      }
+
+    }
+
+  })
+
+
+  ## UI$columns: Select two columns for ploting----
+  output$column1 <- renderUI({
+    # If missing input, return to avoid error later in function
+    if(is.null(input$file2plot)){
+      df <- f_read_csv("test.csv",
+                       filesep =  input$sep
+      )
+      columns<-names(df)
+    }else{
+      df <- f_read_csv(input$file2plot,
+                       filesep =  input$sep
+      )
+      columns<-names(df)
+    }
+
+    # Create the checkboxes and select them all by default
+    selectInput("column1", "Select the X - axis column for plotting",
+                choices  = columns
+    )
+  })
+  output$column2 <- renderUI({
+    # If missing input, return to avoid error later in function
+    if(is.null(input$file2plot)){
+      df <- f_read_csv("test.csv",
+                       filesep =  input$sep
+      )
+      columns<-names(df)
+    }else{
+      df <- f_read_csv(input$file2plot,
+                       filesep =  input$sep
+      )
+      columns<-names(df)
+    }
+
+    # Create the checkboxes and select them all by default
+    selectInput("column2", "Select the Y - axis for plotting",
+                choices  = columns
+    )
+  })
+  ## output$contents: Print the table of a selected uploaded file ----
+  output$contents <- renderTable({
+    # input$file1 will be NULL initially. After the user selects
+    # and uploads a file, head of that data file by default,
+    # or all rows if selected, will be shown.
+
+    req(input$file2plot)
+
+    df <- f_read_csv(input$file2plot,
+                     filesep =  input$sep
+    )
+
+    return(head(df))
+  })
+
+  # Mainpanel:----
+  ## output$summary: Print the summary of a selected uploaded file  ----
+  output$summary <- renderPrint({
+    # input$file1 will be NULL initially. After the user selects
+    # and uploads a file, head of that data file by default,
+    # or all rows if selected, will be shown.
+
+    req(input$file2plot)
+
+    df <- f_read_csv(input$file2plot,
+                     filesep =  input$sep
+    )
+    return(summary(df))
+
+  })
+  ## output$distPloterror: Print the error info for plotting ----
+  output$distPloterror <- renderPrint({
+    # generate bins based on input$bins from ui.R
+    req(input$file2plot)
+    req(input$daterange1)
+    if(is.null(input$file2plot)){
+      df <- f_read_csv("test.csv",
+                       filesep =  input$sep
+      )
+      columns<-names(df)
+    }else{
+      df <- f_read_csv(input$file2plot,
+                       filesep =  input$sep
+      )
+      columns<-names(df)
+    }
+
+    x=df[,input$column1]
+    y=df[,input$column2]
+    xlab=input$column1;ylab = input$column2
+
+    if(input$column1=="Timestamp"){
+      daterange<-input$daterange1
+      if(range(df$Timestamp)[1]>daterange[1]) return(print(paste0("The start date should be later than ",range(df$Timestamp)[1])))
+      if(range(df$Timestamp)[2]<daterange[2]) return(print(paste0("The end date should be earler than ",range(df$Timestamp)[2])))
+      df<-subset(df, Timestamp>=daterange[1] & Timestamp<=daterange[2])
+    }
+  })
+
+  ## output$distPlot: Plot the select file ----
+  output$distPlot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    req(input$file2plot)
+    req(input$daterange1)
+    if(is.null(input$file2plot)){
+      df <- f_read_csv("test.csv",
+                       filesep =  input$sep
+      )
+      columns<-names(df)
+    }else{
+      df <- f_read_csv(input$file2plot,
+                       filesep =  input$sep
+      )
+      columns<-names(df)
+    }
+
+    x=df[,input$column1]
+    y=df[,input$column2]
+    xlab=input$column1;ylab = input$column2
+
+    if(input$column1=="Timestamp"){
+      daterange<-input$daterange1
+      if(range(df$Timestamp)[1]>daterange[1]) return(print(paste0("The start date should be later than ",range(df$Timestamp)[1])))
+      if(range(df$Timestamp)[2]<daterange[2]) return(print(paste0("The end date should be earler than ",range(df$Timestamp)[2])))
+      df<-subset(df, Timestamp>=daterange[1] & Timestamp<=daterange[2])
+      x=df[,input$column1]
+      y=df[,input$column2]
+    }
+
+    if(input$type1=="line"){
+      plot(x,y,xlab=xlab,ylab =ylab,"l")
+      points(x,y)
+    }else if(input$type1=="point")
+      plot(x,y,xlab=xlab,ylab =ylab)
+  },
+  height=800,
+  res = 100)
+
+
+  # Tab: simulation----
+  # Sidepanel: ----
+
+  ## output$uploadfilename:Select a uploaded file for subsetting----
+  output$uploadfilename <- renderUI({
+    # If missing input, return to avoid error later in function
+    if(is.null(input$files)){
+      uploadedfiles<-"test.csv"
+    }else{
+      uploadedfiles <-dir("tmp/",'.csv')[-which(dir("tmp/",'.csv')=="test.csv")]
+    }
+
+    # Create the checkboxes and select them all by default
+    selectInput("uploadfilename", "Select the upload filename for simulation",
+                choices  = uploadedfiles,
+                selected = "test.csv"
+    )
+  })
+
+
+  ## Action: Subset data by Station ID and period----
+  observeEvent(input$subdata, # subsetting input data
+               output$subsetdata<-renderText({
+                 print("Subseting data has started, please wait .....")
+                 f_subset(input$StationID,input$subsetdaterange,input$uploadfilename)
+                 })
+  )
+
+  ## Action: Print the input data info ----
+  observeEvent(input$printinfo,
+               output$inputfilesummary<-renderPrint({
+                 if(!file.exists(paste0("www/",input$inputfiles,".csv"))) return(print(paste0("There is no ", input$inputfiles," input")))
+                 df<-read.csv(paste0("www/",input$inputfiles,".csv"),sep=",",stringsAsFactors = F)
+                 if("Timestamp" %in% names(df)) df$Timestamp<-as.Date(df$Timestamp,format="%Y-%m-%d")
+                 print("Summary informaiton")
+                 print(summary(df))
+                 print("Header informaiton")
+                 print(head(df))
+                 print("Tailer informaiton")
+                 print(tail(df))
+               })
+
+  )
+
+  ## Mainpanel: ----
+  ## Action: Merge climate and lai data ----
+  observeEvent(input$mergeinput,
+               output$mergeinputout<-renderPrint({
+                 f_merge_input()
+               })
+  )
+  ## Action: Calculate WUE based on vegetation ratio ----
+  observeEvent(input$calwue,
+               output$wues<-renderPrint({
+                 if(!file.exists(paste0("www/LUCC.csv"))) return(print("There is no Landcover input"))
+                 vegratio<-read.csv("www/LUCC.csv",sep=",",stringsAsFactors = F)
+                 f_WaSSI_WUE(vegratio)
+               })
+  )
+
+  ## Action: Calculate ET based on SUN ET function ----
+  observeEvent(input$calsunET,
+               output$ETs<-renderPrint({
+                 if(!file.exists(paste0("www/LUCC.csv"))) return(print("There is no Landcover input"))
+                 vegratio<-read.csv("www/LUCC.csv",sep=",",stringsAsFactors = F)
+                 wues<-f_WaSSI_WUE(vegratio)
+                 if(!file.exists(paste0("www/pars.csv"))) return(print("There is no pars input"))
+                 print("Calculating monthly ET based on SUN's function")
+                 pars<-read.csv("www/pars.csv",header = T)
+                 pars<-c("LAT"=pars$LAT,"LONG"=pars$LONG,"ALT"=pars$ALT,wues)
+                 print(pars)
+                 load("www/data_TS.Rdata")
+                 ET<-f_SUN_ET(data_TS,pars)
+                 save(ET,file = "www/ET.RData")
+               })
+  )
+
+  ## Action: Transfer monthly input to daily input ----
+  observeEvent(input$mon2day,
+               output$mon2dayout<-renderPrint({
+                 load("www/ET.RData")
+                 data_TS_daily<-month2daily(ET,fields = c("P","Q","PET","E"),ts = T)
+                 save(data_TS_daily,file="www/data_TS_daily.RData")
+               })
+  )
+
+  ## Action: Calculate snow ----
+  observeEvent(input$calsnow,
+               output$calsnowout<-renderPrint({
+                 load("www/data_TS_daily.RData")
+                 snow_out<-f_snow_calculation(data_daily)
+                 save(snow_out,file="www/snow_out.RData")
+               })
+  )
+
+  ## Action: Run model ----
+  observeEvent(input$runmodel,
+               output$ning2<-renderPrint({
+                 print("Simulation has started, please wait .....")
+                 load("www/data_TS_daily.RData")
+                 soil<-as.vector(read.csv("www/SOIL.csv"))
+                 soil_pars<-list("uztwm" = 1, "uzfwm" = 150, "uzk" = 0.1, "pctim" = 1e-06, "adimp" = 0,
+                                 "zperc" = 28.8997, "rexp" = 5, "lztwm" = 205.652, "lzfsm" = 758.774,
+                                 "lzfpm" = 1000, "lzsk" = 0.149213, "lzpk" = 0.00607691, "pfree" = 0.582714)
+                 soil_pars[names(soil)]<-soil[names(soil)]
+
+                 if(as.integer(format(range(index(data_TS_daily))[1],"%Y"))>input$simulatedaterange[1]) return(print(paste0("The start date should be later than ",range(index(data_TS_daily))[1])))
+                 if(as.integer(format(range(index(data_TS_daily))[2],"%Y"))<input$simulatedaterange[2]) return(print(paste0("The end date should be earler than ",range(index(data_TS_daily))[2])))
+
+                 outp<-f_WaSSI_Q(data_TS_daily,soil_pars,
+                                 calibrate =F,
+                                 y_s = input$simulatedaterange[1],
+                                 y_e = input$simulatedaterange[2]
+                                 )
+                 save(outp,file="www/outp.RData")
+                 })
+  )
+
+  ## Action: Plot the basic result----
+  observeEvent(input$plotres,{
+
+    output$plotresout<-renderPlot({
+      req(input$plotvars)
+      load("www/outp.RData")
+      out<-merge(outp$Input,outp$Out)
+      ind<-which(index(out)>= as.POSIXct(paste0(input$plotdaterange[1],"-01-01")) & index(out)<= as.POSIXct(paste0(input$plotdaterange[2],"-12-31")))
+      plot(out[ind,input$plotvars],main = "",xlab = "Time")
+      },
+      height=1000,
+      res = 100)
+  })
+
+  ## Action: select time period for calibration and validation----
+
+
+
+  } # END
+)
---
+++ ui.R
@@ -0,0 +1,246 @@
+#
+# This is the user-interface definition of a dWaSSI-C web application. You can
+# run the application by clicking 'Run App' above.
+#
+
+library(shiny)
+
+# Define UI for dWaSSI-C application
+shinyUI(
+  navbarPage(
+    "dWaSSIC model",
+    # Tab: Upload data ----
+    tabPanel("Upload input data",
+             sidebarLayout(
+               sidebarPanel(
+                 # Application title
+                 titlePanel("Select inputs data for dWaSSIC model"),
+                 ## HTML: introduction of the input data
+                 HTML("<p>The input data of dWaSSIC model includes:</p>
+                        <ol>
+                        <li>Watershed infomation</li>
+                        <li>Monthly climate time series for each HRU</li>
+                        <li>Monthly LAI data</li>
+                        <li>Landcover</li>
+                        <li>Soil parameters</li>
+                        <li>Shapefiles of your study area (Optional)</li>
+                        </ol>"),
+                 tags$hr(),
+                 # Action: Read input data ----
+                 actionButton("readdata","Read all input data!"),
+                 HTML("<p>You can prepare those files as a 'csv/txt' format.</p>
+                      "),
+                 ## Choose each file----
+                 fileInput("Input_HRUinfo", "Watershed info [HRUID, Latitude, Longitude, Elevation_m and Area_m2]"),
+                 fileInput("Input_climate", "Monthly Climate [HRUID, Year, Month, Precip_mm, Tmean_C]"),
+                 fileInput("Input_LAI", "Monthly LAI [HRUID, Year, Month, LAI]"),
+                 fileInput("Input_landinfo", "Land cover info [HRUID, IGBP,Ratio]"),
+                 fileInput("Input_soil", "Soil info [HRUID, uztwm,uzfwm,uzk,zperc,rexp,lztwm,lzfsm,lzfpm,lzsk,lzpk,pfree]")
+
+               ),
+
+               # MainPanel for upload data----
+               mainPanel(
+                 ## Output: Summary of the selected plotting file----
+                 h2("The summary of the input file"),
+                 verbatimTextOutput("printsummary")
+               )
+             )
+    ),
+    # Tab: Plot data ----
+    tabPanel("Plot input data",
+             sidebarLayout(
+               sidebarPanel(
+                 # Application title
+
+                 h2("Upload watershed Shapefile"),
+                 HTML("<p>Please chose a '*gml' file to upload.</p>
+                      "),
+                 fileInput('Input_basin', 'Choose watershed shapefile', multiple=FALSE, accept="gml"),
+                 # Action: Plot basin ----
+                 actionButton("plotbasin","Plot the uploaded watershed!"),
+
+                 ## Plot the information----
+                 h2("Select a dataset to plot"),
+                 ## Input: Select a file to plot----
+                 selectInput("daname2plot", "Name of the dataset.",
+                             choices=c("Climate","LAI")),
+                 # Input: Year for plotting ---
+                 sliderInput("plotyrrange", "Select the year range", 1970, 2017, value = c(2000, 2010)),
+                 # Input: Month ----
+                 checkboxGroupInput("plotmonths","Select the months to plot",c(1:12)),
+
+                 # Action: plot input data ----
+                 actionButton("plotdata","Plot the selected input data!")
+               ),
+
+               # MainPanel for upload data----
+               mainPanel(
+                 ## Output: Head of the selected plotting file----
+                 h2("The Map of the Watershed"),
+                 leafletOutput("basinmap"),
+
+                 ## Output: Plot of the selected two columns----
+                 h2("Please select two columns to plot the data"),
+                 verbatimTextOutput("distPloterror"),
+                 plotOutput("Plotinput")
+               )
+             )
+    ),
+    # Tab: Simulation-----
+    tabPanel("Run the simulation",
+             sidebarLayout(
+               sidebarPanel(
+                 # Application title
+                 titlePanel("Select the station and timeperiod for simulation"),
+                 ## HTML: Introduction ----
+                  HTML("<p>Please select a station for simulation:</p>
+                      <ol>
+                      <li>Climate data</li>
+                      <li>LAI data</li>
+                      <li>Landcover</li>
+                      <li>Soil</li>
+                      </ol>"),
+                 tags$hr(),
+                 ## Input: Select a Station ----
+                 textInput("StationID", "Type the Station ID for simulation","1"
+                 ),
+                 ## UIoutput: select a file to subset ----
+                 uiOutput("uploadfilename"),
+
+                 # Input: Time range for subsetting ----
+                 dateRangeInput("subsetdaterange","Select the date range for simulation"),
+
+                 # Action: Subset data ----
+                 actionButton("subdata","Run Subset"),
+                 verbatimTextOutput("subsetdata"),
+
+                 h2("Select a file to print the basic information"),
+                 # Input: select an input data to print  ----
+                 selectInput("inputfiles", "Select a file to print the information",c("Climate","LAI","SOIL","LUCC","Flow"),"Climate"
+                 ),
+
+
+                 tags$hr()
+                  ),
+
+                 # Mainpanel: of Simulation----
+                 mainPanel(
+
+                   h2("Print the input data informaiton"),
+                   # Action: pint the selected inputdata----
+                   actionButton("printinfo","Print input data"),
+                   verbatimTextOutput("inputfilesummary"),
+
+                   # Horizontal line ----
+                   tags$hr(),
+                   h2("Merge climate and LAI input data"),
+                   # Action: Merge Climate and LAI data----
+                   actionButton("mergeinput","Merge inputs"),
+                   verbatimTextOutput("mergeinputout"),
+
+                   tags$hr(),
+                   h2("Calculate water use efficiency using vegetation ratio"),
+                   # Action: Calculate WUE based on vegetation ratio----
+                   actionButton("calwue","Calculate WUE"),
+                   verbatimTextOutput("wues"),
+
+                   h2("Calculate ET using Sun's function"),
+                   # Action: Calculate monthly Sun ET----
+                   actionButton("calsunET","Calculate ET"),
+                   verbatimTextOutput("ETs"),
+
+                   h2("Transfering monthly data to daily data"),
+                   # Action: Convert monthly input to daily input ----
+                   actionButton("mon2day","Convert monthly input to daily"),
+                   verbatimTextOutput("mon2dayout"),
+
+                   h2("Calculate snow and effective rainfall using daily data"),
+                   # Action: Calculate snow with daily input ----
+                   actionButton("calsnow","Calculate snow"),
+                   verbatimTextOutput("calsnowout"),
+
+                   tags$hr(),
+                   h2("Simulate water and carbon processes"),
+                   # Input: Checkbox if calibate model ----
+                   checkboxInput("Calibration", "Calibration", FALSE),
+                  # Input: Simulation period
+                   sliderInput("simulatedaterange", "Year simulation", 1970, 2017, value = c(2000, 2010)),
+
+                  ## Action: run model
+                  actionButton("runmodel","Run Simulation"),
+
+                   tags$hr(),
+                   h2("Plot water and carbon processes"),
+                  # Input: Result plot variables ----
+                  checkboxGroupInput("plotvars","Select the variables to plot",c(input_vars,result_vars[1:8])),
+
+                  # Input: Year for plotting ---
+                  sliderInput("plotdaterange", "Year plot", 1970, 2017, value = c(2000, 2010)),
+
+                  # Action: Plot simulated result ---
+                   actionButton("plotres","Plot"),
+
+                  # Output: ploted simulated result
+                   plotOutput("plotresout")
+                 )
+              )
+        ),
+    tabPanel("Interprate the result"),
+    navbarMenu("Export result",
+               tabPanel("Export water",
+
+                        # Sidebar layout with input and output definitions ----
+                        sidebarLayout(
+
+                          # Sidebar panel for inputs ----
+                          sidebarPanel(
+
+                            # Input: Choose dataset ----
+                            selectInput("water", "Choose a dataset:",
+                                        choices = c("rock", "pressure", "cars")),
+
+                            # Button
+                            downloadButton("downloadData", "Download")
+
+                          ),
+
+                          # Main panel for displaying outputs ----
+                          mainPanel(
+
+                            tableOutput("table")
+
+                          )
+                        )
+
+               ),
+               tabPanel("Export Climate",
+
+                        # Sidebar layout with input and output definitions ----
+                        sidebarLayout(
+
+                          # Sidebar panel for inputs ----
+                          sidebarPanel(
+
+                            # Input: Choose dataset ----
+                            selectInput("dataset1", "Choose a dataset:",
+                                        choices = c("rock", "pressure", "cars")
+                            ),
+
+                            # Button
+                            downloadButton("downloadData1", "Download")
+
+                          ),
+
+                          # Main panel for displaying outputs ----
+                          mainPanel(
+
+                            tableOutput("table1")
+
+                          )
+                        )
+
+               )
+    )
+  )
+)
