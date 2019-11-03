38fa0b9c0c59a64f7281fd9610929c81f4af74ec
Ning <ln1267@gmail.com>
2019-11-03 11:02:20 -0500

    move some unuseful uis and revised some

--- ui.R
+++ ui.R
@@ -58,6 +58,10 @@ shinyUI(
                       "),
                  fileInput('Input_basin', 'Choose watershed shapefile', multiple=FALSE, accept="gml"),
 
+                 # Action: Plot basin ----
+                 actionButton("processrasters","process input"),
+                 # Action: Process input ----
+                 actionButton("plotrasterdata","Plot the input rasters!"),
                  #h2("Upload all original raster files"),
                  HTML("<p>Please chose a '*.tif' or '*.nc' monthly stacked precipitation and temperation file to upload.</p>
                       "),
@@ -67,12 +71,7 @@ shinyUI(
                  fileInput('Input_lc_raster', 'Choose land cover raster file', multiple=FALSE, accept="tif"),
                  fileInput('Input_lai_raster', 'Choose LAI raster file', multiple=FALSE, accept="tif"),
                  fileInput('Input_soil_raster', 'Choose Soil raster file', multiple=FALSE, accept="tif"),
-                 fileInput('Input_imp_raster', 'Choose impverious raster file [Optional]', multiple=FALSE, accept="tif"),
-
-                 # Action: Plot basin ----
-                 actionButton("processrasters","process input"),
-                 # Action: Process input ----
-                 actionButton("plotrasterdata","Plot the input rasters!")
+                 fileInput('Input_imp_raster', 'Choose impverious raster file [Optional]', multiple=FALSE, accept="tif")
 
                ),
 
@@ -104,6 +103,8 @@ shinyUI(
                  ## Input: Select a file to plot----
                  selectInput("daname2plot", "Name of the dataset.",
                              choices=c("Climate","LAI")),
+                 ## Input: Select a variable ----
+                 textInput("plotvar", "Type the variable name for ploting","Ppt_mm"),
                  # Input: Year for plotting ---
                  sliderInput("plotyrrange", "Select the year range", 1970, 2017, value = c(2000, 2010)),
                  # Input: Month ----
@@ -118,7 +119,7 @@ shinyUI(
 
                  ## Output: Plot of the selected two columns----
                  h2("Please select two columns to plot the data"),
-                 verbatimTextOutput("prntplotinfo"),
+                 verbatimTextOutput("prntplottinginfo"),
                  verbatimTextOutput("distPloterror"),
 
                  plotOutput("Plotinput",height = 800)
@@ -141,10 +142,8 @@ shinyUI(
                       </ol>"),
                  tags$hr(),
                  ## Input: Select a Station ----
-                 textInput("StationID", "Type the Station ID for simulation","1"
+                 textInput("StationID", "Type the BasinID for simulation","1"
                  ),
-                 ## UIoutput: select a file to subset ----
-                 uiOutput("uploadfilename"),
 
                  # Input: Time range for subsetting ----
                  dateRangeInput("subsetdaterange","Select the date range for simulation"),
@@ -152,66 +151,23 @@ shinyUI(
                  # Action: Subset data ----
                  actionButton("subdata","Run Subset"),
                  verbatimTextOutput("subsetdata"),
-
-                 h2("Select a file to print the basic information"),
-                 # Input: select an input data to print  ----
-                 selectInput("inputfiles", "Select a file to print the information",c("Climate","LAI","SOIL","LUCC","Flow"),"Climate"
-                 ),
-
-
-                 tags$hr()
+                 # Input: Simulation period
+                 sliderInput("simulatedaterange", "Year simulation", 1970, 2017, value = c(2000, 2010)),
+                 # Input: Checkbox if calibate model ----
+                 checkboxInput("Calibration", "Calibration", FALSE),
+                 ## Action: run model
+                 actionButton("runmodel","Run Simulation")
                   ),
 
                  # Mainpanel: of Simulation----
                  mainPanel(
 
-                   h2("Print the input data informaiton"),
-                   # Action: pint the selected inputdata----
-                   actionButton("printinfo","Print input data"),
-                   verbatimTextOutput("inputfilesummary"),
-
-                   # Horizontal line ----
-                   tags$hr(),
-                   h2("Merge climate and LAI input data"),
-                   # Action: Merge Climate and LAI data----
-                   actionButton("mergeinput","Merge inputs"),
-                   verbatimTextOutput("mergeinputout"),
-
-                   tags$hr(),
-                   h2("Calculate water use efficiency using vegetation ratio"),
-                   # Action: Calculate WUE based on vegetation ratio----
-                   actionButton("calwue","Calculate WUE"),
-                   verbatimTextOutput("wues"),
-
-                   h2("Calculate ET using Sun's function"),
-                   # Action: Calculate monthly Sun ET----
-                   actionButton("calsunET","Calculate ET"),
-                   verbatimTextOutput("ETs"),
-
-                   h2("Transfering monthly data to daily data"),
-                   # Action: Convert monthly input to daily input ----
-                   actionButton("mon2day","Convert monthly input to daily"),
-                   verbatimTextOutput("mon2dayout"),
-
-                   h2("Calculate snow and effective rainfall using daily data"),
-                   # Action: Calculate snow with daily input ----
-                   actionButton("calsnow","Calculate snow"),
-                   verbatimTextOutput("calsnowout"),
-
-                   tags$hr(),
                    h2("Simulate water and carbon processes"),
-                   # Input: Checkbox if calibate model ----
-                   checkboxInput("Calibration", "Calibration", FALSE),
-                  # Input: Simulation period
-                   sliderInput("simulatedaterange", "Year simulation", 1970, 2017, value = c(2000, 2010)),
-
-                  ## Action: run model
-                  actionButton("runmodel","Run Simulation"),
 
                    tags$hr(),
                    h2("Plot water and carbon processes"),
                   # Input: Result plot variables ----
-                  checkboxGroupInput("plotvars","Select the variables to plot",c(input_vars,result_vars[1:8])),
+                  checkboxGroupInput("plotvars","Select the variables to plot",c(input_vars,result_vars[1:8]),inline = T),
 
                   # Input: Year for plotting ---
                   sliderInput("plotdaterange", "Year plot", 1970, 2017, value = c(2000, 2010)),
